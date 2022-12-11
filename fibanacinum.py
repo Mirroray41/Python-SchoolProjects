c3ffdd8bdcd96867cc385d82669a657cd521f2bd
ZAppFireCZ <87991171+ZAppFireCZ@users.noreply.github.com>
2022-12-11 11:50:10 +0100

    Update fibanacinum.py

--- fibanacinum.py
+++ fibanacinum.py
@@ -1,10 +1,8 @@
-mem, a0, a1 = 0, 0, 1
-count = int(input("How many?:")) 
+mem, a0, a1, count = 0, 0, 1, int(input("How many?:"))
 if count < 0: count *= -1
 print("0\n1")
 while count >> 1:
-    count-=1
-    mem = a0 + a1
-    a0, a1 = a1, mem
+    mem =  a0 + a1
+    a0, a1, count = a1, mem, count - 1
     print(mem)
 print("\n" + str(a1))
